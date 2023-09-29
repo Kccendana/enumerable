d609f235da8963c02caecbad813498a398fa480d
okari <nyandika15@gmail.com>
2023-09-29 11:59:39 +0300

    fix failing linters

---  main.rb
+++  main.rb
@@ -1,15 +1,13 @@
-# main.rb
-
 require_relative 'my_list'
 require_relative 'my_enumerable'
 
 list = MyList.new([1, 2, 3, 4, 5])
 
-puts "all?: #{list.all? { |item| item > 0 }}"
+puts "all?: #{list.all?(&:positive?)}"
 # Output: true
 
-puts "any?: #{list.any? { |item| item < 0 }}"
+puts "any?: #{list.any?(&:negative?)}"
 # Output: false
 
-puts "filter: #{list.filter { |item| item.even? }}"
+puts "filter: #{list.filter(&:even?)}"
 # Output: [2, 4]
